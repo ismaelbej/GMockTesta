0bcdb2ef013629391337e76a76c66f55962ff0b2
Ismael Bejarano <ismaelbej@gmail.com>
2014-02-04 23:22:19 -0300

    Agrego ejemplo usando template mixin

--- main.cpp
+++ main.cpp
@@ -42,6 +42,57 @@ TEST(Hello,World)
   EXPECT_EQ(42, concrete.GetValue());
 }
 
+class IFaceEx : public IFace
+{
+public:
+  virtual ~IFaceEx() {}
+
+  virtual double DoOtherStuff(double) = 0;
+};
+
+template <typename T>
+class MockIFaceT : public T
+{
+public:
+  MOCK_METHOD1_T(DoThis,int(int));
+};
+
+class MockIFaceEx : public MockIFaceT<IFaceEx>
+{
+public:
+  MOCK_METHOD1(DoOtherStuff,double(double));
+};
+
+class ConcreteClassEx
+{
+public:
+  ConcreteClassEx(IFaceEx* pIFace, int val)
+  {
+    _value = val + 2 * pIFace->DoOtherStuff(val);
+  }
+  int GetValue() const
+  {
+    return _value;
+  }
+private:
+  int _value;
+};
+
+TEST(Hello,WorldT)
+{
+  using ::testing::Return;
+  using ::testing::_;
+  MockIFaceEx mockIFaceEx;
+  ON_CALL(mockIFaceEx, DoThis(_))
+    .WillByDefault(Return(0));
+  ON_CALL(mockIFaceEx, DoOtherStuff(_))
+    .WillByDefault(Return(0));
+  EXPECT_CALL(mockIFaceEx, DoOtherStuff(123))
+    .WillOnce(Return(111));
+  ConcreteClassEx concreteEx(&mockIFaceEx, 123);
+  EXPECT_EQ(345, concreteEx.GetValue());
+}
+
 int main(int argc, char** argv)
 {
   ::testing::InitGoogleMock(&argc, argv);
